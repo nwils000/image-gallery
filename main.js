b7ef0c98804e8a7a5ff81b1e2f5b6e2aa7d728d2
nwils000 <nwils000@gmail.com>
2024-03-26 12:10:00 -0400

    Added the ability to change the main image on click

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
+const fullImage = document.querySelector('.full-img');
 
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
@@ -14,6 +15,8 @@ const altText = {
   pic5: 'Butterfly on leaf',
 };
 
+/* Adding images to displayImage on click */
+
 /* Looping through images */
 for (let i = 0; i < images.length; i++) {
   let img = document.createElement('img');
@@ -22,6 +25,10 @@ for (let i = 0; i < images.length; i++) {
 
   altIdentifier = `pic${i + 1}`;
   img.setAttribute('alt', altText[altIdentifier]);
+
+  img.addEventListener('click', () => {
+    displayedImage.setAttribute('src', `images/${images[i]}`);
+  });
 
   thumbBar.appendChild(img);
 }
@@ -37,9 +44,6 @@ const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
-
-// Loop through the array of filenames, and for each one, insert an <img>
-// element inside the thumb-bar <div> that embeds that image in the page along with its alternative text.
 
 // Add a click event listener to each <img> inside the thumb-bar <div> so that,
 // when they are clicked, the corresponding image and alternative text are displayed in the displayed-img <img> element.
