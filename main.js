a8e2f790c0922a0037f84b93a87f4b1494661131
nwils000 <nwils000@gmail.com>
2024-03-26 11:07:53 -0400

    Added images array in main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const thumbBar = document.querySelector('.thumb-bar');
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 
